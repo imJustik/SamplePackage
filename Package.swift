7a9dbc50d0322e9192a47bf6bef6b2ab492d7791
imkuznetsov <kuznetsov.i@indriver.com>
2024-03-27 23:04:22 +0200

    up version

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         .target(
             name: "SamplePackage"),
         
-            .binaryTarget(name: "App", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.1/App.xcframework.zip", checksum: "b49faec341ec057b4de7148a7f2838e202675620a0956b389cf30138439b43c0"),
+            .binaryTarget(name: "App", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.3/App.xcframework.zip", checksum: "b49faec341ec057b4de7148a7f2838e202675620a0956b389cf30138439b43c0"),
             
-            .binaryTarget(name: "Flutter", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.1/Flutter.xcframework.zip", checksum: "2bca72b1fcdab5713ff8b52c2c509b104ce1c8f3c3e5cb735bbab98d78282cbb"),
+            .binaryTarget(name: "Flutter", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.3/Flutter.xcframework.zip", checksum: "2bca72b1fcdab5713ff8b52c2c509b104ce1c8f3c3e5cb735bbab98d78282cbb"),
             
-            .binaryTarget(name: "FlutterPluginRegistrant", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.1/FlutterPluginRegistrant.xcframework.zip", checksum: "4d722988ac2685c391f56f07b1a7e7fc3e2cee38094ab4a0b1b16048eeb14207"),
+            .binaryTarget(name: "FlutterPluginRegistrant", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.3/FlutterPluginRegistrant.xcframework.zip", checksum: "4d722988ac2685c391f56f07b1a7e7fc3e2cee38094ab4a0b1b16048eeb14207"),
             
-            .binaryTarget(name: "test_plugin", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.1/test_plugin.xcframework.zip", checksum: "a0634c433cf07872dcc80f0cca344513599faf4ba9574d922dd9e98af9e948d1"),
+            .binaryTarget(name: "test_plugin", url: "https://github.com/imJustik/SamplePackage/releases/download/0.0.3/test_plugin.xcframework.zip", checksum: "a0634c433cf07872dcc80f0cca344513599faf4ba9574d922dd9e98af9e948d1"),
         
         
         .testTarget(
